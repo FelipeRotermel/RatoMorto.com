3b7827b5684fc18f206fd6fac6a768fe07d6dac1
Felipe Rotermel <feliperotermell@gmail.com>
2022-10-03 21:49:03 -0300

    aaaaaaaaaaaaa

--- src/main.js
+++ src/main.js
@@ -4,16 +4,11 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
-import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
 
 import './assets/main.css'
 
 const app = createApp(App)
-app.use(BootstrapIconsPlugin);
-
 app.use(router)
 app.use(createPinia())
 
 app.mount('#app')
-
-window.$ = window.jQuery = require('jquery');
